fec30ae64d28de46fead4ddf74b42b0ca82c9ea6
Divyanshu <divyanshu.upadhyay.civ17@itbhu.ac.in>
2021-08-27 00:37:07 +0530

    Updated file

--- imports/ui/components/link_list.js
+++ imports/ui/components/link_list.js
@@ -7,7 +7,7 @@ class LinkList extends Component{
   renderRows(){
     return this.props.links.map(link =>{
       const {url, clicks, token} = link;  //ES6 destructuring
-      const shortLink = `http://localhost:3000/${token}`;
+      const shortLink = `https://shortclone.herokuapp.com/${token}`;
 
       return(
         <tr key={token}>
