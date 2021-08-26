c14e9ba41fe4c11da7f930ac2d12c3231dd93b22
Divyanshu <divyanshu.upadhyay.civ17@itbhu.ac.in>
2021-08-26 23:22:23 +0530

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,24 @@
+//Code executes only at the client side
+//Any JavaScript code here will automatically run for us
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import './main.html';            //Importing the html file
+import Header from '../imports/ui/components/header';
+import LinkCreate from '../imports/ui/components/link_create';
+import { Links } from '../imports/api/links';
+import LinkList from '../imports/ui/components/link_list';
+
+const App = ()=>{
+  return (
+    <div>
+      <Header />
+      <LinkCreate />
+      <LinkList />
+    </div>
+  );
+};
+
+Meteor.startup(()=>{
+  ReactDOM.render(<App/>, document.querySelector('.render-target'));
+});
---
+++ server/main.js
@@ -0,0 +1,39 @@
+import { Meteor } from 'meteor/meteor';
+import { Links } from '../imports/api/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
+
+Meteor.startup(() => {
+  Meteor.publish('links',function(){
+    return Links.find({});
+  });
+});
+
+// executed when user visits valid links like
+// 'localhost:3000/abcd'
+function onRouteRedirect(req, res, next){
+  // extract the token from the url and try to find the
+  // matching url in the links collection
+  const link = Links.findOne({token : req.params.token});
+  if(link){
+    // if we find a link object, redirect the user to long URL
+		Links.update(link, {$inc: {clicks:1}});
+    res.writeHead(307,{'Location':link.url});
+    res.end();
+  }
+  else{
+    // if we don't find the link object,
+    // redirect the user to normal React page
+    next();
+  }
+}
+
+// localhost:3000   -- NO MATCH
+// localhost:3000/absc/xasdccv  -- NO MATCH
+// localhost:3000/abcd   -- MATCH
+
+const middleware = ConnectRoute(function(router){
+  router.get('/:token', onRouteRedirect);
+})
+
+WebApp.connectHandlers.use(middleware);
